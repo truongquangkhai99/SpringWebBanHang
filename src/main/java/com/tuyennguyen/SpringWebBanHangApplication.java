0972f3cc0dee6b39dd22a80dcec9f1b7f5abd142
Admin <nguyenvantuyen6789@gmail.com>
2021-10-02 09:11:15 +0700

    Xong! Tạo Dự Án Đầu Tiên

---
+++ src/main/java/com/tuyennguyen/SpringWebBanHangApplication.java
@@ -0,0 +1,13 @@
+package com.tuyennguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBanHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebBanHangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuyennguyen/SpringWebBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyennguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebBanHangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
