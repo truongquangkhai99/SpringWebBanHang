74aa901cded9a8846772bc0818492e3df9bf3190
Admin <nguyenvantuyen6789@gmail.com>
2021-10-09 07:28:24 +0700

    Xong! Xoá 1 Số Import Không Dùng Đến

--- src/main/java/com/tuyennguyen/controller/ProductController.java
+++ src/main/java/com/tuyennguyen/controller/ProductController.java
@@ -2,20 +2,14 @@ package com.tuyennguyen.controller;
 
 import com.tuyennguyen.entity.Product;
 import com.tuyennguyen.serivce.ProductService;
-import com.tuyennguyen.util.StatusEnum;
 import com.tuyennguyen.util.UtilCon;
 import com.tuyennguyen.util.UtilFun;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
