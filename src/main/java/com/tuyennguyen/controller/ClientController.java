3be4bd416db230580d45d8d5dd84c6b74d459854
Admin <nguyenvantuyen6789@gmail.com>
2021-10-14 16:52:35 +0700

    Xong! Tạo Liên Hệ Thành Công

--- src/main/java/com/tuyennguyen/controller/ClientController.java
+++ src/main/java/com/tuyennguyen/controller/ClientController.java
@@ -9,29 +9,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
-public class HomeController extends WebController {
+public class ClientController extends WebController {
 
-    Logger logger = LoggerFactory.getLogger(HomeController.class);
-    private static final String mainObject = "home";
+    Logger logger = LoggerFactory.getLogger(ClientController.class);
+    private static final String mainObject = "client";
 
     @Autowired
     private MenuDongService mainService;
 
     @GetMapping(value = "/")
     public String goHome(Model model) {
-        logger.debug("Go to " + UtilCon.toClient(mainObject));
+        logger.debug("Go to " + UtilCon.toClient("home"));
         setCommon(model);
 
         List<MenuDong> listMenuDong = mainService.findAll();
         model.addAttribute("listMenuDong", listMenuDong);
 
         return UtilCon.toClient(mainObject);
+    }
+
+    @GetMapping(value = "/lien-he")
+    public String goLienHe(Model model) {
+        logger.debug("Go to " + UtilCon.toClient("lien-he"));
+        setCommon(model);
+
+        List<MenuDong> listMenuDong = mainService.findAll();
+        model.addAttribute("listMenuDong", listMenuDong);
+
+        return UtilCon.toClient("lien-he");
     }
 	
 }
