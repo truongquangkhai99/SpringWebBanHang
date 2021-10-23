43caf0032795c92614b18284bd1b4dfbe6417a64
Admin <nguyenvantuyen6789@gmail.com>
2021-10-24 00:58:16 +0700

    Xong! Spring Security Login Cho Admin, User

--- src/main/java/com/tuyennguyen/controller/AdminController.java
+++ src/main/java/com/tuyennguyen/controller/AdminController.java
@@ -1,27 +1,33 @@
 package com.tuyennguyen.controller;
 
-import com.tuyennguyen.serivce.UserService;
 import com.tuyennguyen.util.UtilCon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController extends WebController {
 
     Logger logger = LoggerFactory.getLogger(AdminController.class);
     private static final String mainObject = "admin";
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/admin")
     public String admin(Model model) {
+        System.out.println("he he");
         logger.debug("Go to " + UtilCon.toAdmin());
         setCommon(model);
 
         return UtilCon.toAdmin(mainObject);
+    }
+
+    @RequestMapping("/admin-login")
+    public RedirectView localRedirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://localhost:8080/login");
+        return redirectView;
     }
 
 }
